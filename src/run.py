cf73f7f14fb7a88d619f693149856eadb3e4c16e
AlexDo1 <alex.dolich@gmail.com>
2023-03-20 17:03:11 +0100

    wip sellonlatbox

--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime as dt
+import subprocess
 from pprint import pprint
 
 from json2args import get_parameter
@@ -7,16 +8,14 @@ from json2args import get_parameter
 # parse parameters
 kwargs = get_parameter()
 
-# check if a toolname was set in env
-toolname = os.environ.get('TOOL_RUN', 'foobar').lower()
+# check if a toolname was set in env, default tool: sellonlatbox
+toolname = os.environ.get('TOOL_RUN', 'sellonlatbox').lower()
 
 # switch the tool
-if toolname == 'foobar':
-    # RUN the tool here and create the output in /out
-    print('This toolbox does not include any tool. Did you run the template?\n')
-    
-    # write parameters to STDOUT.log
-    pprint(kwargs)
+if toolname == 'sellonlatbox':
+    print(kwargs)
+    # run the command
+    subprocess.Popen(f"cdo sellonlatbox,{kwargs['min_lon']},{kwargs['max_lon']},{kwargs['min_lat']},{kwargs['max_lat']} {kwargs['infile']} /out/outfile.nc")
 
 # In any other case, it was not clear which tool to run
 else:
